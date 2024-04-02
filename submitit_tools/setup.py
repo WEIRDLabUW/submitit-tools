1924ce5b89a6825354e07fcada0b5b9bd3de19cf
Jacob Berg <jacob.berg.seattle@gmail.com>
2024-04-01 23:17:24 -0700

    Added a setup.py :(

---
+++ submitit_tools/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='submitit_tools',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A package to manage submitit jobs',
+    author='Weird Lab',
+)
