f3345bb4bcd0d6d0ec13f27ab3b804adcb6fe5f2
jacob33 <hockeyviolin@gmail.com>
2024-04-08 12:33:26 -0700

    Configured the slurm executor correctly

--- configs/submitit_config.py
+++ configs/submitit_config.py
@@ -3,19 +3,21 @@ from dataclasses import dataclass
 
 @dataclass
 class SubmititExecutorBaseConfig:
-    # Don't changet these
-    slurm_gpus_per_node = 1  # The number of gpus that the job will take
-    slurm_gpus_per_task = 1  # The number of gpus per task
+    # Don't changet these probably
+    slurm_account: str = "weirdlab"  # This is the account to which the job is ran from
     slurm_ntasks_per_node = 1  # The number of tasks per node
 
 
 @dataclass
 class SubmititExecutorConfig(SubmititExecutorBaseConfig):
     root_folder: str = "default_root_folder"  # This is the root folder to where submitit output is saved
-    slurm_account: str = "weirdlab"  # This is the account to which the job is ran from
     slurm_name: str = "experiment"  # This is the name of the job that shows up on squeue
     timeout_min: int = (4 * 60) - 1  # This is the timeout in minutes
     cpus_per_task: int = 4  # This is the number of cpus per task
     mem_gb: int = 10  # This is the amount of ram required
     slurm_partition: str = "ckpt"  # This is the partition to which the job is submitted
-    # slurm_array_parallelism: int = 10
+    
+    # This is the extra paramater dictionary where args become SBATCH commands
+    slurm_additional_parameters: dict = {
+        "gpus": "a40:1" # This is gpu_type:number
+    }
