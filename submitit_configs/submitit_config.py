262792eb1ff490ba75a57db31f025fa52c363e08
Jacob Berg <jacob.berg.seattle@gmail.com>
2024-04-26 12:27:34 -0700

    Refactored

--- submitit_configs/submitit_config.py
+++ submitit_configs/submitit_config.py
@@ -1,18 +1,19 @@
 from dataclasses import dataclass, field
 
 
+# @dataclass
+# class SubmititExecutorBaseConfig:
+#     # Don't change these probably
+
+
+
 @dataclass
-class SubmititExecutorBaseConfig:
-    # Don't change these probably
+class SubmititExecutorConfig(SubmititExecutorBaseConfig):
     slurm_account: str = "weirdlab"  # This is the account to which the job is ran from
     slurm_ntasks_per_node = 1  # The number of tasks per node
     slurm_gpus_per_node = 1  # The number of gpus that the job will take
     slurm_gpus_per_task = 1  # The number of gpus per task
     slurm_nodes = 1  # The number of nodes utilized
-
-
-@dataclass
-class SubmititExecutorConfig(SubmititExecutorBaseConfig):
     root_folder: str = "default_root_folder"  # This is the root folder to where submitit output is saved
     slurm_name: str = "experiment"  # This is the name of the job that shows up on squeue
     timeout_min: int = (4 * 60) - 1  # This is the timeout in minutes
