e0134718bee868809691723bbb06fa6f2188e130
Jacob33 <hockeyviolin@gmail.com>
2024-10-07 21:15:05 -0700

    updated init..py and added the functionjob utilities

--- submitit_tools/jobs/__init__.py
+++ submitit_tools/jobs/__init__.py
@@ -1,3 +1,4 @@
 from .run_state import SubmititState
 from .base_classes import BaseJob, FailedJobState
-from .utils import run_nvidia_smi
+from .default_jobs import create_function_job
+from .utils import run_nvidia_smi, grid_search_job_configs
